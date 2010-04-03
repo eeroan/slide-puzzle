55946dc1f2cb1528bf535af22d2f96507038482d
U-eeroan-PC\eeroan <eeroan@eeroan-PC.(none)>
2010-04-03 17:57:08 +0300

    refactoring

--- palapeli.js
+++ palapeli.js
@@ -9,39 +9,29 @@ $('#board').click(function(e) {
 	var box = $(e.target);
 	if(box.hasClass('piece')) {
 		var old = box.data('pos');
-		var freePos = nextToFree(old);
+		var freePos = nextToFree(old.x, old.y);
 		if(freePos) {
 			var newX = freePos.x;
 			var newY = freePos.y;
 			table[newX][newY]=table[old.x][old.y];
 			table[old.x][old.y]=null;
-			box.css(pos(newX,newY)).data('pos',{x:newX,y:newY});
+			box.animate(pos(newX,newY)).data('pos',{x:newX,y:newY});
 		}
 	}
 });
 
-function nextToFree(pos) {
-	var x=pos.x;
-	var y=pos.y;
-	var positions = [{x:x,y:y+1},{x:x+1,y:y},{x:x,y:y-1},{x:x-1,y:y}];
-	for(var i in positions) {
-		var nextX = positions[i].x;
-		var nextY = positions[i].y;
-		var freePos = getIfFree(nextX,nextY);
+function nextToFree(x, y) {
+	var sides = [{x:x,y:y+1},{x:x+1,y:y},{x:x,y:y-1},{x:x-1,y:y}];
+	for(var i in sides) {
+		var side = sides[i];
+		var freePos = getIfFree(side.x, side.y);
 		if(freePos) return freePos;
 	}
 	return null;
 }
-function getIfFree(x,y) {
-	if(x<0 || x>=splitX || y<0 || y>=splitY || table[x][y] != null) {
-		return null;
-	} else {
-		return {x:x, y:y};
-	}
+function getIfFree(x,y) { 
+	return (x<0 || x>=splitX || y<0 || y>=splitY || table[x][y] != null) ? null :{x:x, y:y};
 }
-function pos(slotX, slotY) { return {left:(slotX*width)+'px',top:(slotY*height)+'px'}}
-function getBoard() {return $("#board").css('width',boardX+'px').css('height',boardY+'px');}
-
 function initTable() {
 	var board =  getBoard();
 	var pieces = getPieces();
@@ -75,5 +65,6 @@ function getPieces() {
 function piece(x,y) {
 	return $('<div>').addClass('piece').width(width).height(height)
 	.css('background-position','-'+(x*width)+'px -'+(y*height)+'px');
-	
 }
+function pos(slotX, slotY) { return {left:(slotX*width)+'px',top:(slotY*height)+'px'}}
+function getBoard() {return $("#board").width(boardX).height(boardY);}
