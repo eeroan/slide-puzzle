b2a21b254bb792a6d21b09bdc5b427a3637adcb8
eeroan <eea@iki.fi>
2010-05-25 01:15:16 +0300

    refactor tests

--- test/spec/unit/spec.slide-puzzle.js
+++ test/spec/unit/spec.slide-puzzle.js
@@ -1,12 +1,17 @@
-describe('slide-puzzle', function() {
+JSpec.describe('slide-puzzle', function() {
   before_each(function() {
-    dom = $(JSpec.parseXML(fixture('slide-puzzle')));
-    container = dom.find('#board');
+    ctx = $('#board');
+    ctx.empty();
+    ctx.slidePuzzle({image:'../../main/Koala.jpg', split:3});
   });
 
   describe('when initializing', function() {
     it('container is visible', function() {
-      expect(container).to(be_visible);
+      expect(ctx).to(be_visible);
+    });
+
+    it('game has right amount of pieces', function() {
+      expect(ctx.find('.box')).to(have_length, 8);
     });
   });
 });
