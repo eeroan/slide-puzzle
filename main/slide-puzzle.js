ea59c38213ea2960568abcab25d3117294233c83
Eero Anttila <eea@iki.fi>
2011-02-05 01:59:01 +0200

    get rid of semicolons

--- main/slide-puzzle.js
+++ main/slide-puzzle.js
@@ -1,30 +1,30 @@
 //TODO theme, difficulty, levels, high score, timer, moves, mouse over
 
 $.fn.slidePuzzle = function(opts) {
-  var emptyCell;
-  var container = this;
-  init(opts);
+  var emptyCell
+  var container = this
+  init(opts)
 
   function init(opts) {
     if(opts.width && opts.height) {
-      initWithImageDimensions(opts, {x:opts.width, y:opts.height});
+      initWithImageDimensions(opts, {x:opts.width, y:opts.height})
     } else {
-      container.append('<img>', {src:opts.image,class:imgHolder});
+      container.append('<img>', {src:opts.image,class:imgHolder})
       $('.imgHolder', container).load(function() {
-        var image = $('.imgHolder', container).get(0);
-        var board = {x:image.width, y:image.height};
-        initWithImageDimensions(opts, board);
-      });
+        var image = $('.imgHolder', container).get(0)
+        var board = {x:image.width, y:image.height}
+        initWithImageDimensions(opts, board)
+      })
     }
   }
 
   function initWithImageDimensions(opts, board) {
-    var boxInfo = {};
-    boxInfo.gridSize = {x:opts.split, y:opts.split};
-    boxInfo.boxSize = {x:divideFor('x'), y:divideFor('y')};
-    boxInfo.image = opts.image;
-    var table = createTable(board, boxInfo);
-    initEvents(table, boxInfo);
+    var boxInfo = {}
+    boxInfo.gridSize = {x:opts.split, y:opts.split}
+    boxInfo.boxSize = {x:divideFor('x'), y:divideFor('y')}
+    boxInfo.image = opts.image
+    var table = createTable(board, boxInfo)
+    initEvents(table, boxInfo)
     function divideFor(axis) {
       return parseInt(board[axis]/boxInfo.gridSize[axis])
     }
@@ -41,23 +41,23 @@ $.fn.slidePuzzle = function(opts) {
 
 
   function initEvents(table, boxInfo) {
-    var click = getBoard().toObservable('click').Select(eventTarget).Where(isBox).Select(boxPosition);
-    var keyPress = $(document).toObservable('keyup').Select(function(e) {return getMovableBox(e.keyCode)});
+    var click = getBoard().toObservable('click').Select(eventTarget).Where(isBox).Select(boxPosition)
+    var keyPress = $(document).toObservable('keyup').Select(function(e) {return getMovableBox(e.keyCode)})
 
     click.Subscribe(function(from) {
       if (findNextToFree(from, boxInfo.gridSize, table.grid)) {
         moveBox(from, emptyCell, table, boxInfo)
       }
-    });
+    })
 
     keyPress.Subscribe(function(from) {
       if(from && !isOutOfBounds(from, boxInfo.gridSize)) {
-        moveBox(from, emptyCell, table, boxInfo);
+        moveBox(from, emptyCell, table, boxInfo)
       }
-    });
+    })
     function getMovableBox(key) {
-      var keyCodes = {37:'LEFT', 38:'UP', 39:'RIGHT', 40:'DOWN'};
-      return neighboursOf(emptyCell)[keyCodes[key]];
+      var keyCodes = {37:'LEFT', 38:'UP', 39:'RIGHT', 40:'DOWN'}
+      return neighboursOf(emptyCell)[keyCodes[key]]
     }
 
     function isBox(box) {
@@ -74,90 +74,90 @@ $.fn.slidePuzzle = function(opts) {
   }
 
   function moveBox(from, to, tableObj, boxInfo) {
-    var table = tableObj.grid;
-    var box = table[from.x][from.y];
-    table[to.x][to.y] = box;
-    table[from.x][from.y] = null;
-    emptyCell = from;
-    box.animate(pos(to, boxInfo.boxSize), 300, validate).data('pos', to);
+    var table = tableObj.grid
+    var box = table[from.x][from.y]
+    table[to.x][to.y] = box
+    table[from.x][from.y] = null
+    emptyCell = from
+    box.animate(pos(to, boxInfo.boxSize), 300, validate).data('pos', to)
     function validate() {
-      if(isDone(table)) gameCompleted(boxInfo, tableObj.hiddenBox);
+      if(isDone(table)) gameCompleted(boxInfo, tableObj.hiddenBox)
     }
   }
 
   function gameCompleted(boxInfo, hiddenBox) {
-    var end = boxInfo.gridSize;
+    var end = boxInfo.gridSize
     hiddenBox.css(pos({x:end.x - 1,y:end.y - 1}, boxInfo.boxSize)).hide().appendTo(getBoard()).fadeIn(2000, function() {
-      alert("Congratulations! Play again?");
-      document.location = document.location.href;
-    });
+      alert("Congratulations! Play again?")
+      document.location = document.location.href
+    })
   }
 
   function isDone(table) {
     for(var x in table) {
       for(var y in table[x]) {
-        var slot = table[x][y];
+        var slot = table[x][y]
         if(slot) {
-          var box = slot.data('origPos');
+          var box = slot.data('origPos')
           if(box.x != x || box.y != y) {
-            return false;
+            return false
           }
         }
       }
     }
-    return true;
+    return true
   }
 
   function findNextToFree(old, grid, table) {
-    var sides = neighboursOf(old);
+    var sides = neighboursOf(old)
     for(var i in sides) {
-      var side = sides[i];
-      if(!isOutOfBounds(side, grid) && table[side.x][side.y] == null) return side;
+      var side = sides[i]
+      if(!isOutOfBounds(side, grid) && table[side.x][side.y] == null) return side
     }
-    return null;
+    return null
   }
 
   function isOutOfBounds(box, grid) {
-    return box.x < 0 || box.x >= grid.x || box.y < 0 || box.y >= grid.y;
+    return box.x < 0 || box.x >= grid.x || box.y < 0 || box.y >= grid.y
   }
 
   function createTable(boardDim, boxInfo) {
-    var board = getBoard().width(boardDim.x).height(boardDim.y);
-    var boxes = getBoxes(boxInfo);
-    var hiddenBox = boxes.pop();
-    var table = [];
+    var board = getBoard().width(boardDim.x).height(boardDim.y)
+    var boxes = getBoxes(boxInfo)
+    var hiddenBox = boxes.pop()
+    var table = []
     for(var x = 0; x < boxInfo.gridSize.x; x++) {
-      table[x] = [];
+      table[x] = []
       for(var y = 0; y < boxInfo.gridSize.y; y++) {
         if(boxes.length) {
-          var slot = {x:x, y:y};
-          var box = boxes.pop().css(pos(slot, boxInfo.boxSize)).data('pos', slot);
-          board.append(box);
-          table[x][y] = box;
+          var slot = {x:x, y:y}
+          var box = boxes.pop().css(pos(slot, boxInfo.boxSize)).data('pos', slot)
+          board.append(box)
+          table[x][y] = box
         } else {
-          table[x][y] = null;
+          table[x][y] = null
           emptyCell = {x:x,y:y}
         }
       }
     }
-    return {grid:table, hiddenBox: hiddenBox};
+    return {grid:table, hiddenBox: hiddenBox}
   }
 
   function getBoxes(boxInfo) {
-    var boxes = [];
+    var boxes = []
     for(var x = 0; x < boxInfo.gridSize.x; x++) {
       for(var y = 0; y < boxInfo.gridSize.y; y++) {
-        boxes.push(createBox(x, y, boxInfo));
+        boxes.push(createBox(x, y, boxInfo))
       }
     }
-    return boxes;
+    return boxes
   }
 
   function createBox(x, y, boxInfo) {
     with(boxInfo) {
       return $('<div>').addClass('box').width(boxSize.x).height(boxSize.y)
         .css('background', 'url(' + image + ') -' + (x*boxSize.x) + 'px -' + (y*boxSize.y) + 'px')
-        .data('origPos', {x:x, y:y});
+        .data('origPos', {x:x, y:y})
     }
   }
 
@@ -166,6 +166,6 @@ $.fn.slidePuzzle = function(opts) {
   }
 
   function getBoard() {
-    return container;
+    return container
   }
-};
+}
